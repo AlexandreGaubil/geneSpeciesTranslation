d8ae410e3fa61459e23c26d5e6a029907dfd4fce
Alexandre Gaubil <agaubil@gmail.com>
2021-12-25 09:49:16 +0200

    Added tests, gitignore

---
+++ R/test_convert_genes.R
@@ -0,0 +1,8 @@
+library(geneSpeciesTranslation)
+
+df <- read.csv('../test-df.csv')
+conversion_file_path <- '../mouse-human-genes.csv'
+
+test_that("convert_genes test", {
+  expect_equal(convert_genes(df, conversion_file_path, "MouseSymbol", "HumanSymbol"))
+})
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(geneSpeciesTranslation)
+
+test_check("geneSpeciesTranslation")
